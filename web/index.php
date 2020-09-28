ee6df5cbf173ab1f5d09eb3f839f04764d9660eb
Samuel <samuel.avendanof@pca.edu.co>
2020-09-27 23:31:39 -0500

    Actualización conexión con HEROKU index.php

--- web/index.php
+++ web/index.php
@@ -1,25 +1,46 @@
-<?php
+<? php
 
-require('../vendor/autoload.php');
+use  Symfony \ Component \ HttpFoundation \ Request ;
 
-$app = new Silex\Application();
-$app['debug'] = true;
+require ( '../vendor/autoload.php' );
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+$ aplicación = nueva  Silex \ Aplicación ();
+$ aplicación [ 'depuración' ] = verdadero ;
+
+// Registrar el servicio de registro de monólogos
+$ aplicación -> registro ( nuevo  Silex \ Provider \ MonologServiceProvider (), array (
+  'monolog.logfile' => 'php: // stderr' ,
 ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+// Registrar la representación de la vista
+$ aplicación -> registrar ( nuevo  Silex \ Provider \ TwigServiceProvider (), array (
+    'twig.path' => __DIR__. '/ vistas' ,
 ));
 
-// Our web handlers
+// Nuestros manipuladores web
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+$ aplicación -> get ( '/' , function () use ( $ app ) {
+  $ app [ 'monolog' ] -> addDebug ( 'salida de registro.' );
+  return  $ aplicación [ 'ramita' ] -> render ( 'index.twig' );
 });
 
-$app->run();
+
+// Ruta de demostración, para validar que se recibe (n) dato (s) y se responde con este mismo
+$ app -> post ( '/ enviarDato' , function ( Request  $ request ) use ( $ app ) {
+   devolver  $ solicitud ;
+});
+
+
+// Ruta de demostración, se recibe (n) dato (s) y se manipulan
+$ app -> post ( '/ modificarDato' , function ( Request  $ request ) use ( $ app ) {
+   	$ nombre = $ solicitud -> get ( 'nombre' );
+	$ respuesta = "Hola" . $ nombre ;
+   	return  $ respuesta ;
+});
+
+// Ruta de demostración, se recibe (n) dato (s) y se manipulan
+$ app -> post ( '/ postArduino' , function ( Request  $ request ) use ( $ app ) {
+   	devuelve  "OK" ;
+});
+
+$ aplicación -> ejecutar ();
